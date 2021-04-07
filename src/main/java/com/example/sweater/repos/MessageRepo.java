8852d62af3b14b952decae6d51b3b7b11c7bc180
vald3r <vald3r@gmail.com>
2021-04-07 17:24:27 +0300

    first commit

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,8 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public class MessageRepo extends CrudRepository<Message, Long> {
+
+}

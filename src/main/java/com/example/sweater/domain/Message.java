a01f434a0ea979e991cd61e20c58a2a78550fdd7
vald3r <vald3r@gmail.com>
2021-04-25 22:02:56 +0300

    add file and static

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -13,6 +13,7 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+    private String filename;
 
 
     public Message() {
@@ -59,5 +60,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
